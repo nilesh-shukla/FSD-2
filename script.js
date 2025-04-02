b2f78a9c440de34c014a1c07286611b4a18b9eb6
Nilesh Shukla <shuklanilesh810@gmail.com>
2025-04-02 11:28:11 +0530

    FSD-2 NodeJS Demo

---
+++ script.js
@@ -0,0 +1,26 @@
+// function sayHello(){
+//     let you = prompt("What's your name?");
+//     console.log("Hello", you + "!");
+// }
+
+// let varContainingFunction = function(){
+//     let varInFunction = "I in funtion";
+//     console.log("hi there!", varInFunction);
+// }
+
+// function tester(para1, para2) {
+//     return para1 + " " + para2;
+// }
+// const arg1 = "Argument 1";
+// const arg2 = "Argument 2";
+// console.log(tester(arg1, arg2));
+
+function addTwoNumber(x, y)
+{
+    console.log(x+y);
+}
+let var1 = prompt("Enter a:", "0");
+let var2 = prompt("Enter b:", "0");
+let x = parseInt(var1);
+let y = parseInt(var2);
+addTwoNumber(x, y);
