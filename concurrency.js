5f01f17c64db70139a94811957fe9682873f1f8e
Nilesh Shukla <shuklanilesh810@gmail.com>
2025-05-02 12:25:05 +0530

    ASYNC-AWAIT PROGRAMMING, EVENT LOOP AND CALL STACK

--- concurrency.js
+++ concurrency.js
@@ -2,11 +2,12 @@
 // A callback is a function that is passed as an argument to another function.
 // function doSomething(callback){
 //     callback();
+//     console.log("Hello World");
 // }
 // function sayHi(){
 //     console.log("Hi");
 // }
-// doSomething(sayHi);
+// doSomething(sayHi); Output: Hi \n Hello World
 
 // function judge(grade){
 //     switch(true){
@@ -67,26 +68,60 @@
 //     }
 // );
 
-const promise = new Promise((resolve, reject) => {
-    // resolve("success!!");
-    reject("error!!");
-})
-    .then(value => {
-        console.log(value);
-        return "we";
-    })  
-    .then(value => {
-        console.log(value);
-        return "can";
-    })
-    .then(value => {
-        console.log(value);
-        return "chain";
-    })
-    .then(value => {
-        console.log(value);
-        return "promises";
-    })
-    .catch(value => {
-        console.log(value);
-    })
+// const promise = new Promise((resolve, reject) => {
+//     // resolve("success!!");
+//     reject("error!!");
+// })
+//     .then(value => {
+//         console.log(value);
+//         return "we";
+//     })  
+//     .then(value => {
+//         console.log(value);
+//         return "can";
+//     })
+//     .then(value => {
+//         console.log(value);
+//         return "chain";
+//     })
+//     .then(value => {
+//         console.log(value);
+//         return "promises";
+//     })
+//     .catch(value => {
+//         console.log(value);
+//     })
+
+
+//** ASYN AND AWAIT */
+// With async keyword, we can make a function return a Promise. This makes Promise nicer to read and look a lot like synchronous code. 
+//The more powerful await keyword to wait until a Promise is done, await only works in an asynchronous function. 
+
+// function saySomething(x){
+//     return new Promise(resolve => {
+//         setTimeout(() => {
+//             resolve(" something " + x);
+//         }, 2000);
+//     });
+// }
+// async function talk(x){
+//     const words = await saySomething(x);
+//     console.log(words);
+// }
+// talk(2);
+// talk(4);
+// talk(8);
+
+//** EVENT LOOP: 
+
+//** CALL STACK:
+
+// console.log("Hi there");
+// setTimeout(() => console.log("Sorry I am late"), 1000);
+// console.log(add(4, 5));
+
+// function add(x, y){
+//     return x+y;
+// }
+// The setTimeout() task gets outsourced to browser's web API, and when it is done this appears in a special place: the callback queue. When the call stack is empty, the event loop will check the callback queue for work to do. If there are any callbacks waiting, they'll be executed, one by one. After every action , the event loop will check the cll stack for words
+
